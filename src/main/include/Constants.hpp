8c1ea10c825d7ec0d7f090b412a7740f855b8d8e
Cody L. Wellman <cody@codexmicro.systems>
2023-10-26 18:28:24 -0400

    Fix constants

---
+++ src/main/include/Constants.hpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2023 Team 3602 All rights reserved. This work is
+ * licensed under the terms of the MIT license which can be found
+ * in the root directory of this project.
+ */
+
+#pragma once
+
+namespace ExampleConstants {
+
+}  // namespace ExampleConstants
